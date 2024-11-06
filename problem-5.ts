88b790ada5830c1400c1e8423984f6fadb3c9d53
codermahbub20 <mkrefat5@gmail.com>
2024-11-07 00:02:14 +0600

    Problem 5 solved

---
+++ problem-5.ts
@@ -0,0 +1,13 @@
+{
+
+const getProperty = <T,K extends keyof T>(obj:T,key:K):T[K]=>{
+    return obj[key];
+}
+
+const person = { name: "Alice", age: 30 };
+
+const Result = getProperty(person,'name')
+
+console.log(Result)
+
+}
