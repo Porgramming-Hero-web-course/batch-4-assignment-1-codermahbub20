852c1893616565cb2d6e06792587ec18e2213c2f
codermahbub20 <mkrefat5@gmail.com>
2024-11-07 22:21:44 +0600

    Problem -8 solved

---
+++ problem-8.ts
@@ -0,0 +1,11 @@
+{
+
+    const validateKeys = <T>(obj: T, keys: (keyof T)[]): boolean => {
+        return keys.every(key => key in obj);
+    }
+
+    const person = { name: "Alice", age: 25, email: "alice@example.com" };
+    const result = (validateKeys(person, ["name", "age"]));
+    console.log(result)
+
+}
